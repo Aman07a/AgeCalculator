8ae703006cc622f402df868088e04a38873cc6ba
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-27 09:28:08 +0200

    The Age Calculator Solution

--- age-calculator.test.ts
+++ age-calculator.test.ts
@@ -7,12 +7,14 @@
 import { createAgeCalculator } from "./age-calculator";
 
 describe("agecalculator", () => {
-  describe("already had birthday this year", () => {
+  describe("has had birthday", () => {
     test.each([
       { birthDate: "1979/02/15", targetDate: "2000/03/21", expected: 21 },
       { birthDate: "1950/01/31", targetDate: "2001/03/21", expected: 51 },
       { birthDate: "2015/03/10", targetDate: "2022/03/21", expected: 7 },
       { birthDate: "2000/02/29", targetDate: "2022/03/01", expected: 22 },
+      { birthDate: "2004/02/28", targetDate: "2022/03/01", expected: 18 },
+      { birthDate: "2000/02/29", targetDate: "2008/02/29", expected: 8 },
     ])(
       "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
       ({ birthDate, targetDate, expected }) => {
@@ -26,11 +28,12 @@ describe("agecalculator", () => {
     );
   });
 
-  describe("has not had birthday this year", () => {
+  describe("has not had birthday", () => {
     describe("birthday in later month", () => {
       test.each([
         { birthDate: "1979/02/15", targetDate: "2000/01/01", expected: 20 },
         { birthDate: "2015/03/10", targetDate: "2022/02/28", expected: 6 },
+        { birthDate: "2004/02/29", targetDate: "2005/03/01", expected: 1 },
       ])(
         "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
         ({ birthDate, targetDate, expected }) => {
@@ -47,6 +50,7 @@ describe("agecalculator", () => {
     describe("birthday in same month", () => {
       test.each([
         { birthDate: "1950/01/31", targetDate: "2000/01/01", expected: 49 },
+        { birthDate: "2000/02/29", targetDate: "2022/02/28", expected: 21 },
       ])(
         "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
         ({ birthDate, targetDate, expected }) => {
