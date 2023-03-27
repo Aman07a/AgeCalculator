fdf8a6dc7ac40270da9c0a8a112d9fbdab8275fa
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-27 09:14:05 +0200

    Worked Example: The Age Calculator

--- age-calculator.test.ts
+++ age-calculator.test.ts
@@ -12,6 +12,7 @@ describe("agecalculator", () => {
       { birthDate: "1979/02/15", targetDate: "2000/03/21", expected: 21 },
       { birthDate: "1950/01/31", targetDate: "2001/03/21", expected: 51 },
       { birthDate: "2015/03/10", targetDate: "2022/03/21", expected: 7 },
+      { birthDate: "2000/02/29", targetDate: "2022/03/01", expected: 22 },
     ])(
       "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
       ({ birthDate, targetDate, expected }) => {
@@ -23,5 +24,40 @@ describe("agecalculator", () => {
         expect(actual).toBe(expected);
       }
     );
+  });
+
+  describe("has not had birthday this year", () => {
+    describe("birthday in later month", () => {
+      test.each([
+        { birthDate: "1979/02/15", targetDate: "2000/01/01", expected: 20 },
+        { birthDate: "2015/03/10", targetDate: "2022/02/28", expected: 6 },
+      ])(
+        "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
+        ({ birthDate, targetDate, expected }) => {
+          // Arrange
+          const sut = createAgeCalculator();
+          // Act
+          const actual = sut(new Date(birthDate), new Date(targetDate));
+          // Assert
+          expect(actual).toBe(expected);
+        }
+      );
+    });
+
+    describe("birthday in same month", () => {
+      test.each([
+        { birthDate: "1950/01/31", targetDate: "2000/01/01", expected: 49 },
+      ])(
+        "birthdate: $birthDate, targetDate: $targetDate, expected: $expected",
+        ({ birthDate, targetDate, expected }) => {
+          // Arrange
+          const sut = createAgeCalculator();
+          // Act
+          const actual = sut(new Date(birthDate), new Date(targetDate));
+          // Assert
+          expect(actual).toBe(expected);
+        }
+      );
+    });
   });
 });
--- age-calculator.ts
+++ age-calculator.ts
@@ -1,5 +1,15 @@
 export function createAgeCalculator() {
   return function (birthDate: Date, targetDate: Date) {
-    return targetDate.getFullYear() - birthDate.getFullYear();
+    const completedYears = targetDate.getFullYear() - birthDate.getFullYear();
+    if (hasHadBirthday(birthDate, targetDate)) return completedYears;
+    return completedYears - 1;
   };
+}
+
+function hasHadBirthday(birthDate: Date, targetDate: Date) {
+  return (
+    birthDate.getMonth() < targetDate.getMonth() ||
+    (birthDate.getMonth() === targetDate.getMonth() &&
+      birthDate.getDate() <= targetDate.getDate())
+  );
 }
